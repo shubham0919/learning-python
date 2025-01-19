714a4ecd5f07e98272350f8f1d385dfa52a8e686
Shubham Sonwane <shubham@seepossible.com>
2025-01-20 04:59:33 +0530

    typecasting

---
+++ typecasting.py
@@ -0,0 +1,18 @@
+# Typecasting = the process of converting one data type to another data type
+#               str(), int(), float(), bool()
+
+name = "Shubham"
+age = 23
+height = 5.8
+is_student = False
+
+name = bool(name)
+age = str(age)
+height = int(height)
+is_student = int(is_student)
+
+# Printing 
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Height: {height}")
+print(f"Is Student: {is_student}")
