9ec45570906d0ee7b820d122020de5578acf6b92
Shubham Sonwane <shubham@seepossible.com>
2025-01-20 04:51:40 +0530

    variables

---
+++ variable.py
@@ -0,0 +1,21 @@
+# Variable = A conatiner for a value ( string , integer , float , boolean )
+#            A variable behaves as it was the value it contains
+
+# String
+first_name = "Shubham"
+last_name = "Sonwane"
+
+# Integer
+age = 23
+
+# Float
+height = 5.8
+
+# Boolean
+is_student = False
+
+# Printing the variables
+print(f"Hi I am {first_name} {last_name}")
+print(f"I am {age} years old")
+print(f"My height is {height}")
+print(f"Am I a student ? {is_student}")
